d1008122add3bd1aec46a05d03c478d12baec3a1
WebplatF <webplatf@gmail.com>
2026-07-04 18:57:55 +0530

    change in apply request method

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ $router->group(['prefix' => 'api', 'middleware' => 'apikey'], function () use ($
     $router->get('/course_details', ['middleware' => 'token', 'uses' => 'Admin\CourseController@getCourseWithLesson']);
     $router->post('/unlock_video', ['middleware' => 'token', 'uses' => 'User\UserController@unlockVideo']);
     $router->post('/video_status', ['middleware' => 'token', 'uses' => 'User\UserController@videoUpdate']); 
-    $router->post('/apply_coupon', ['middleware' => 'token', 'uses' => 'Coupon\CouponController@applyCoupon']);
+    $router->get('/apply_coupon', ['middleware' => 'token', 'uses' => 'Coupon\CouponController@applyCoupon']);
     $router->group(['prefix' => 'admin', 'middleware' => 'token'], function () use ($router) {
         $router->post('/add_banner', 'Admin\HomePageController@addBanner');
         $router->post('/add_demo', 'Admin\HomePageController@addDemoVideo');

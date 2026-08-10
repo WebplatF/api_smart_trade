efaf4aa1bf33cc1192d792fa4ad5a7177f06118d
WebplatF <webplatf@gmail.com>
2026-08-10 17:21:39 +0530

    event triger

--- app/Http/Controllers/Webhooks/WebhooksController.php
+++ app/Http/Controllers/Webhooks/WebhooksController.php
@@ -15,6 +15,9 @@ class WebhooksController extends Controller
     {
         try {
             $payload = $request->all();
+            Log::info('Kinescope webhook', [
+                'message' => $payload['event']
+            ]);
             if (($payload['event'] ?? null) !== 'media.update.status') {
                 Log::error('Kinescope webhook error', [
                     'message' => $payload['event']

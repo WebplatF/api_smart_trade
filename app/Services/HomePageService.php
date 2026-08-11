461283c2d4d7d0dbdba1ac9589d620ce93a431cd
WebplatF <webplatf@gmail.com>
2026-08-11 18:40:01 +0530

    spelling mistake

--- app/Services/HomePageService.php
+++ app/Services/HomePageService.php
@@ -152,7 +152,7 @@ class HomePageService
                     // $image_path = $this->signCdnUrl(path: $item->image->media_url, ttl: 300,);
                     $response['banner'][] = [
                         'title' => $item->title,
-                        'path' => $$item->image->media_url
+                        'path' => $item->image->media_url
                     ];
                 }
                 if ($item->type === 'video' && $item->video) {

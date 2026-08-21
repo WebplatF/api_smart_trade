c1c9ce62900a69541090e30f17c9af8b74c6fdc9
WebplatF <webplatf@gmail.com>
2026-08-21 10:50:20 +0530

    wallet correction

--- app/Services/TradeEntryService.php
+++ app/Services/TradeEntryService.php
@@ -110,17 +110,21 @@ class TradeEntryService
                     if ($tradeEdit->win_loss === $tradeEntryEditModel->winLoss) {
                         if ($tradeEdit->win_loss == "WIN") {
                             $actualBal = $wallet->amount - $oldAmount + $tradeAmt;
-                            $action = 'deposite';
+                            $walletReduce = $oldAmount + $tradeAmt;
+                            $action = 'withdraw';
                         } else {
                             $actualBal = $wallet->amount + $oldAmount - $tradeAmt;
-                            $action = 'withdraw';
+                            $walletReduce = $oldAmount - $tradeAmt;
+                            $action = 'deposite';
                         }
                     } else {
                         if ($tradeEdit->win_loss === 'WIN' && $tradeEntryEditModel->winLoss === 'LOSS') {
                             $actualBal = $wallet->amount - $oldAmount - $tradeAmt;
+                            $walletReduce = $oldAmount - $tradeAmt;
                             $action = 'withdraw';
                         } else {
                             $actualBal = $wallet->amount + $oldAmount + $tradeAmt;
+                            $walletReduce = $oldAmount + $tradeAmt;
                             $action = 'deposite';
                         }
                     }
@@ -148,7 +152,7 @@ class TradeEntryService
                     $this->walletService->walleteAction(
                         userId: $userId,
                         action: $action,
-                        amount: $tradeAmt,
+                        amount: $walletReduce,
                         isLog: false,
                         date: $tradeEntryEditModel->date
                     );

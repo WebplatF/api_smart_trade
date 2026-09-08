4339180195d08d97bff28237aba9dbb11cbb7c9d
WebplatF <webplatf@gmail.com>
2026-09-08 11:19:16 +0530

    apply chnages

--- app/Resources/CalenderMonthResources.php
+++ app/Resources/CalenderMonthResources.php
@@ -10,11 +10,10 @@ class CalenderMonthResources extends JsonResource
     public function toArray($request)
     {
         return [
-            "date" => $this->date ?? "",
-            "trade_count" =>  $this->trade_count ?? "0",
-            "amount" => $this->amount ?? "0",
-            "win_loss" =>  'WIN' | 'LOSS',
-            "direction" => 'Inward' | 'Outward',
+            'date' => $this['date'] ?? null,
+            'trade_count' => $this['trade_count'] ?? 0,
+            'amount' => $this['amount'] ?? '0.00',
+            'direction' => $this['direction'] ?? null,
         ];
     }
 }
--- app/Resources/WalletSummaryResources.php
+++ app/Resources/WalletSummaryResources.php
@@ -11,15 +11,24 @@ class WalletSummaryResources extends JsonResource
     {
         return [
             "balance" => [
-                "wallet" => $this->wallet ?? "0.00",
-                "total_profits" => $this->totoal_profits ?? "0.00",
-                "total_loss" => $this->total_loss ?? "0.00",
-                "total_withdraw" =>  $this->total_withdraw ?? "0.00",
-                "total_deposit" =>   $this->total_deposit ?? "0.00"
+                "wallet" => $this['wallet'] ?? "0.00",
+
+                "total_profits" => $this['total_profits'] ?? "0.00",
+
+                "total_loss" => $this['total_loss'] ?? "0.00",
+
+                "total_withdraw" => $this['total_withdraw'] ?? "0.00",
+
+                "total_deposit" => $this['total_deposit'] ?? "0.00",
             ],
-            "years" => $this->years ?? [],
-            "months" => $this->months ?? [],
-            "calender_month" => CalenderMonthResources::collection($this->calender_month),
+
+            "years" => $this['years'] ?? [],
+
+            "months" => $this['months'] ?? [],
+
+            "calender_month" => CalenderMonthResources::collection(
+                $this['calender_month'] ?? []
+            ),
         ];
     }
 }
